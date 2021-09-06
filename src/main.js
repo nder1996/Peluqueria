1fac9a746e2bc8b09566f346592e16fcc32fb2df
yojay esteban <yojay1000@gmail.com>
2021-09-05 23:49:02 -0500

    añadido firebase al proyecto

--- src/main.js
+++ src/main.js
@@ -4,6 +4,24 @@ import router from './router'
 import store from './store'
 import vuetify from './plugins/vuetify'
 
+import "firebase/auth";
+import "firebase/firestore";
+import "firebase/storage";
+// https://firebase.google.com/docs/web/setup#available-libraries
+const firebase = require('firebase/app').default
+const firebaseConfig = {
+  apiKey: "AIzaSyBiFJ6xstMaWe9-sTZEYYN3Ea4fxMJyVqo",
+  authDomain: "peluqueria-a6e4d.firebaseapp.com",
+  projectId: "peluqueria-a6e4d",
+  storageBucket: "peluqueria-a6e4d.appspot.com",
+  messagingSenderId: "613793598831",
+  appId: "1:613793598831:web:e3e51a70948c58876d2c9a"
+};
+firebase.initializeApp(firebaseConfig);
+
+export const db = firebase.firestore();
+export const storage = firebase.storage();
+
 Vue.config.productionTip = false
 
 new Vue({
